620efb093a9ff06ad8df6c6835b03842bd1f5a13
Major Hayden <major@mhtx.net>
2026-04-25 15:18:10 -0500

    refactor: delete ptr package
    Signed-off-by: Major Hayden <major@mhtx.net>

--- internal/ptr/ptr.go
+++
@@ -1,8 +0,0 @@
-// Package ptr provides a generic helper for creating pointers to values.
-package ptr
-
-// To returns a pointer to the given value. Useful for initializing
-// pointer fields in struct literals.
-//
-//go:fix inline
-func To[T any](v T) *T { return new(v) }
